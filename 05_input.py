c4fbfd2fed90e31ae88f4f5e8c98ed4fd1f3dbbc
Kriyanshah <kriyanshah0@gmail.com>
2026-08-22 17:42:56 +0530

    Delete 05_input.py

--- 05_input.py
+++
@@ -1,9 +0,0 @@
-"""a = input("no a is :")
-b = input("no b is :")
-print(a+b)""" #this input are considered as string 
-
-
- #to convert string into int do as follows
-a = int(input("no  a is: "))
-b = int(input("no  b is: "))
-print(a+b)
